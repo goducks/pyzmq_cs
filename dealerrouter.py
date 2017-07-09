59ad694ba5915d06120065282557546a4d7987bd
goducks <goducks@users.noreply.github.com>
2017-07-09 16:45:27 -0500

    convert to classes

--- dealerrouter.py
+++ dealerrouter.py
@@ -3,55 +3,50 @@ import time
 from threading import Thread
 from optparse import OptionParser
 
-c_socket = None
-def client(server_port):
-    global c_socket
-    context = zmq.Context().instance()
-    c_socket = context.socket(zmq.DEALER)
-    c_socket.setsockopt(zmq.IDENTITY, b'A')
-    c_socket.connect("tcp://localhost:%s" % server_port)
+class Client():
+    def __init__(self, server_port):
+        context = zmq.Context().instance()
+        self.socket = context.socket(zmq.DEALER)
+        self.socket.setsockopt(zmq.IDENTITY, b'A')
+        self.socket.connect("tcp://localhost:%s" % server_port)
+        print "Client: " + b'A' + " connected to port: " + str(server_port)
 
-def client_run():
-    global c_socket
-    total = 0
-    print "client run start"
-    while True:
-        c_socket.send(b"ready")
-        # We receive one part, with the workload
-        request = c_socket.recv()
-        print "client received: " + request
-        finished = request == b"END"
-        if finished:
-            print("A received: %s" % total)
-            break
-        total += 1
+    def run(self):
+        total = 0
+        print "Client run start"
+        while True:
+            self.socket.send(b"ready")
+            # We receive one part, with the workload
+            request = self.socket.recv()
+            print "client received: " + request
+            finished = request == b"END"
+            if finished:
+                print("A received: %s" % total)
+                break
+            total += 1
+        print "client run end"
 
-    print "client run end"
+class Server():
+    def __init__(self, server_port):
+        context = zmq.Context().instance()
+        self.socket = context.socket(zmq.ROUTER)
+        self.socket.bind("tcp://*:%s" % server_port)
+        print "Server bound to port: " + str(server_port)
 
-server = None
-def server(server_port):
-    global server
-    context = zmq.Context().instance()
-    server = context.socket(zmq.ROUTER)
-    server.bind("tcp://*:%s" % server_port)
+    def run(self):
+        print "Server run start"
+        identity, bounce = self.socket.recv_multipart()
+        print "server received from: " + identity + ", message: " + bounce
 
-def server_run():
-    global server
-    print "server run start"
+        for _ in range(10):
+            # Send two message parts, first the address
+            ident = b'A'
+            # And then the workload
+            work = b"Workload"
+            self.socket.send_multipart([ident, work])
 
-    identity, bounce = server.recv_multipart()
-    print "server received from: " + identity + ", message: " + bounce
-
-    for _ in range(10):
-
-        # Send two message parts, first the address
-        ident = b'A'
-        # And then the workload
-        work = b"Workload"
-        server.send_multipart([ident, work])
-
-    server.send_multipart([b'A', b'END'])
-    print "server run end"
+        self.socket.send_multipart([b'A', b'END'])
+        print "server run end"
 
 if __name__ == "__main__":
     # set up command line arguments using optparse library
@@ -68,19 +63,19 @@ if __name__ == "__main__":
     if (options.server):
         print "--server only mode--"
         # start the server
-        server(server_port)
-        server_run()
+        server = Server(server_port)
+        server.run()
     elif (options.client):
         print "--client only mode--"
         # start the client
-        client(server_port)
-        client_run()
+        client = Client(server_port)
+        client.run()
     else:
         print "--client and server mode--"
         # start both
-        server(server_port)
-        client(server_port)
+        server = Server(server_port)
+        client = Client(server_port)
         time.sleep(0.1)
         print "starting run threads"
-        Thread(target=client_run, args='').start()
-        Thread(target=server_run, args='').start()
+        Thread(target=client.run, args='').start()
+        Thread(target=server.run, args='').start()
