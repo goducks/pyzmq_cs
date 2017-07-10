33b9d733350c597e8df1de1d45aaba43336b85c6
goducks <goducks@users.noreply.github.com>
2017-07-09 19:44:23 -0500

    adding message parsing functions for both client and server

--- dealerrouter.py
+++ dealerrouter.py
@@ -4,12 +4,35 @@ import uuid
 from threading import Thread
 from optparse import OptionParser
 
+###############################################################################
+# This class is used to give switch statement like behavior from C/C++
+# There's no magic here, other than making code elsewhere readable
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        if self.fall or not args:
+            return True
+        elif self.value in args:
+            self.fall = True
+            return True
+        else:
+            return False
+###############################################################################
 class Proto():
     # this is just a namespace for storing protocol IDs
     # TODO: this is ghetto
-    greet =     str(0x00000001)
-    kill =      str(0x0000000A)
-
+    headerlen = 6
+    greet =     b'0x0001'
+    str   =     b'0x0002'
+    kill  =     b'0x000A'
+###############################################################################
 class Client():
     # client registration string
 
@@ -21,7 +44,7 @@ class Client():
         self.socket.setsockopt(zmq.IDENTITY, str(self.id))
         self.socket.connect("tcp://localhost:%s" % server_port)
         # send connection message that will register server with client
-        self.socket.send(Proto.greet)
+        self.send(Proto.greet, '')
         print "Client: " + str(self.id) + " connected to port: " + str(server_port)
 
     def run(self):
@@ -29,15 +52,34 @@ class Client():
         print "Client: start"
         while True:
             # We receive one part, with the workload
-            request = self.socket.recv()
-            print "Client: received " + request
-            finished = request == Proto.kill
-            if finished:
+            msg = self.socket.recv()
+            kill = self.parseMsg(msg)
+            if kill:
                 print("Client: total messages received: %s" % total)
                 break
             total += 1
         print "Client: end"
 
+    def send(self, proto, data):
+        self.socket.send(proto + data)
+
+    def parseMsg(self, msg):
+        ret = False
+        header = msg[0:Proto.headerlen]
+        body = msg[Proto.headerlen:]
+        for case in switch(header):
+            if case(Proto.str):
+                print "Client: string: " + body
+                break
+            if case(Proto.kill):
+                print "Client: kill"
+                ret = True
+                break
+            if case():  # default
+                print "Client: received undefined message!"
+                # TODO: debug
+        return ret
+###############################################################################
 class Server():
     # To track clients, use dictionary of connections where:
     # key = client assigned identity
@@ -58,45 +100,64 @@ class Server():
         print "Server: start"
 
         id, data = self.socket.recv_multipart()
-        # Check for new clients first
-        if not id in self.clientmap:
-            if not data == Proto.greet:
-                print "Server: recv'd message from unregistered client"
-                # TODO: debug
-            else:
-                print "Server: registering new client"
-                self.clientmap[id] = { 'imc' : 1, 'ibr' : len(Proto.greet), 'omc': 0, 'obs' : 0 }
-                print self.clientmap
-        elif data == Proto.greet:
-            print "Server: recv'd duplicate registered client"
-            # TODO: debug
-        else:
-            print "Server: handle message normally"
-            self.parsemsg(id, data)
+        self.parseMsg(id, data)
 
         for _ in range(10):
             for id in self.clientmap.iterkeys():
-                # Send two message parts, first the address
-                # And then the workload
                 work = b"Workload"
-                self.send(id, work)
+                self.send(id, Proto.str, work)
 
+        # Force disconnect/kill all clients
         for id in self.clientmap.iterkeys():
-            self.send(id, Proto.kill)
+            self.send(id, Proto.kill, '')
 
         print "Server: client stats"
         print self.clientmap
         print "Server: end"
 
-    def send(self, id, data):
-        self.socket.send_multipart([id, data])
+    def send(self, id, proto, data):
+        final = proto + data
+        self.socket.send_multipart([id, final])
+        # update send stats
         usage = self.clientmap[id]
         usage['omc'] += 1
-        usage['obs'] += len(data)
+        usage['obs'] += len(final)
 
-    def parsemsg(self, id, data):
-        pass
+    def parseMsg(self, id, msg):
+        header = msg[0:Proto.headerlen]
+        body = msg[Proto.headerlen:]
 
+        # Check if client is registered -- this is messy
+        if not id in self.clientmap and not header == Proto.greet:
+            print "Server: recv'd msg from unregistered client"
+            # TODO: debug
+            return
+
+        for case in switch(header):
+            if case(Proto.greet):
+                self.addClient(id, body)
+                break
+            if case(Proto.str):
+                print "Server: string: " + body
+                break
+            if case():  # default
+                print "Server: received undefined message!"
+                # TODO: debug
+
+        # update receive stats
+        usage = self.clientmap[id]
+        usage['imc'] += 1
+        usage['ibr'] += (Proto.headerlen + len(body))
+
+    def addClient(self, id, body):
+        if id in self.clientmap:
+            print "Server: recv'd duplicate client reg"
+            # TODO: debug
+        else:
+            print "Server: registering new client"
+            self.clientmap[id] = {'imc': 0, 'ibr': 0, 'omc': 0, 'obs': 0}
+            print self.clientmap
+###############################################################################
 if __name__ == "__main__":
     # set up command line arguments using optparse library
     usage = "usage: %prog [options] arg1 arg2"
@@ -128,3 +189,4 @@ if __name__ == "__main__":
         print "starting run threads"
         Thread(target=client.run, args='').start()
         Thread(target=server.run, args='').start()
+###############################################################################
