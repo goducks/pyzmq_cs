bb206e6bbeacc45e77f817437ffe8ab79eba8a42
goducks <goducks@users.noreply.github.com>
2017-07-09 22:32:40 -0500

    adding timer to server for example purposes. TODO: client cleanup on server shutdown/disconnect

--- dealerrouter.py
+++ dealerrouter.py
@@ -1,5 +1,6 @@
 import zmq
 import time
+import timeit as ti
 import uuid
 from threading import Thread
 from optparse import OptionParser
@@ -28,10 +29,11 @@ class switch(object):
 class Proto():
     # this is just a namespace for storing protocol IDs
     # TODO: this is ghetto
-    headerlen = 6
-    greet =     b'0x0001'
-    str   =     b'0x0002'
-    kill  =     b'0x000A'
+    headerlen   = 6
+    greet       =     b'0x0001'
+    str         =     b'0x0002'
+    serverstop  =     b'0x000A'
+    clientstop  =     b'0x000B'
 ###############################################################################
 class Client():
     # client registration string
@@ -43,37 +45,51 @@ class Client():
         self.id = uuid.uuid4()
         self.socket.setsockopt(zmq.IDENTITY, str(self.id))
         self.socket.connect("tcp://localhost:%s" % server_port)
+        # set up a read poller
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
         # send connection message that will register server with client
-        self.send(Proto.greet, '')
+        self.send(Proto.greet)
         print "Client: " + str(self.id) + " connected to port: " + str(server_port)
 
     def run(self):
-        total = 0
         print "Client: start"
+
+        # Client's idle loop
+        timeout = 1
+        total = 0
         while True:
-            # We receive one part, with the workload
-            msg = self.socket.recv()
-            kill = self.parseMsg(msg)
-            if kill:
-                print("Client: total messages received: %s" % total)
-                break
-            total += 1
+            # Read incoming
+            sockets = dict(self.poller.poll(timeout))
+            if self.socket in sockets and sockets[self.socket] == zmq.POLLIN:
+                msg = self.socket.recv()
+                total += 1
+                if not self.parseMsg(msg):
+                    break
+
+            # Send outgoing
+            work = b"workload" + str(total)
+            self.send(Proto.str, work)
+
+        print("Client: total messages received: %s" % total)
         print "Client: end"
 
-    def send(self, proto, data):
+    def send(self, proto, data = b''):
         self.socket.send(proto + data)
 
     def parseMsg(self, msg):
-        ret = False
+        ret = True
         header = msg[0:Proto.headerlen]
         body = msg[Proto.headerlen:]
         for case in switch(header):
             if case(Proto.str):
                 print "Client: string: " + body
                 break
-            if case(Proto.kill):
-                print "Client: kill"
-                ret = True
+            if case(Proto.serverstop):
+                print "Client: serverstop"
+                # Send reply to delete client
+                self.send(Proto.clientstop)
+                ret = False
                 break
             if case():  # default
                 print "Client: received undefined message!"
@@ -93,29 +109,47 @@ class Server():
         context = zmq.Context().instance()
         self.socket = context.socket(zmq.ROUTER)
         self.socket.bind("tcp://*:%s" % server_port)
+        # set up a read poller
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
         self.clientmap = dict()
         print "Server bound to port: " + str(server_port)
 
-    def run(self):
+    def run(self, runtime):
         print "Server: start"
 
-        id, data = self.socket.recv_multipart()
-        self.parseMsg(id, data)
+        # Server's idle loop
+        # Note: artificially running for a fixed number of seconds
+        timeout = 1 # int(1.0/60.0)
+        loopstart = ti.default_timer()
+        running = True
+        while running:
+            # Read incoming
+            sockets = dict(self.poller.poll(timeout))
+            if self.socket in sockets and sockets[self.socket] == zmq.POLLIN:
+                id, data = self.socket.recv_multipart()
+                self.parseMsg(id, data)
 
-        for _ in range(10):
-            for id in self.clientmap.iterkeys():
-                work = b"Workload"
-                self.send(id, Proto.str, work)
+            # Send outgoing
+            for x in range(10):
+                for id in self.clientmap.iterkeys():
+                    work = b"workload" + str(x)
+                    self.send(id, Proto.str, work)
+
+            loopstop = ti.default_timer()
+            if loopstop - loopstart > runtime:
+                running = False
 
         # Force disconnect/kill all clients
+        print "Server: shutting down..."
         for id in self.clientmap.iterkeys():
-            self.send(id, Proto.kill, '')
+            self.send(id, Proto.serverstop)
 
-        print "Server: client stats"
         print self.clientmap
+        self.socket.close()
         print "Server: end"
 
-    def send(self, id, proto, data):
+    def send(self, id, proto, data = b''):
         final = proto + data
         self.socket.send_multipart([id, final])
         # update send stats
@@ -138,7 +172,10 @@ class Server():
                 self.addClient(id, body)
                 break
             if case(Proto.str):
-                print "Server: string: " + body
+                print "Server: string: (" + id + ") " + body
+                break
+            if case(Proto.clientstop):
+                self.removeClient(id, body)
                 break
             if case():  # default
                 print "Server: received undefined message!"
@@ -157,6 +194,14 @@ class Server():
             print "Server: registering new client"
             self.clientmap[id] = {'imc': 0, 'ibr': 0, 'omc': 0, 'obs': 0}
             print self.clientmap
+
+    def removeClient(self, id, body):
+        if id in self.clientmap:
+            print "Server: removing client (" + id + ")"
+            del self.clientmap[id]
+        else:
+            print "Attempt to remove unregistered client"
+            # TODO: debug
 ###############################################################################
 if __name__ == "__main__":
     # set up command line arguments using optparse library
@@ -166,6 +211,8 @@ if __name__ == "__main__":
                       default=False, help="enable server-only run mode")
     parser.add_option("-c", "--client", action="store_true", dest="client",
                       default=False, help="enable client-only run mode")
+    parser.add_option("-r", "--run", type="int", dest="runtime", default=10,
+                      help="set run time [10]")
     (options, args) = parser.parse_args()
 
     server_port = 5556
@@ -174,7 +221,7 @@ if __name__ == "__main__":
         print "--server only mode--"
         # start the server
         server = Server(server_port)
-        server.run()
+        server.run(options.runtime)
     elif (options.client):
         print "--client only mode--"
         # start the client
@@ -188,5 +235,5 @@ if __name__ == "__main__":
         time.sleep(0.1)
         print "starting run threads"
         Thread(target=client.run, args='').start()
-        Thread(target=server.run, args='').start()
+        Thread(target=server.run, args='options.runtime').start()
 ###############################################################################
